edbd9dd22ec49ade5fc9fbb8fe0549953faaf556
Marina <example@email.com>
2022-01-06 21:50:56 +0100

    Modal windows, jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -33,8 +33,31 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
-  });
   
+
+  //Modal
+
+  $('[data-modal=consultation]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+});
+
+$('.button_mini').each(function(i){
+    $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    });
+
+});
+  
+
+  
+
+ 
+
 /* const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -43,4 +66,3 @@ $(document).ready(function(){
     controls: false,
     nav: false //убираем точки
   }); */
-
