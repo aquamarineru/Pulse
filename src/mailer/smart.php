c3116dad252ffe95aac0ab67c3bc96e6542e3059
aqumarine <m_ilyushina@hotmail.com>
2022-01-11 12:37:39 +0100

    Debugging BrowserSyng localhost:3001

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,18 +8,18 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'ssl://smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'aquamarintest@yandex.ru';                 // Наш логин
+$mail->Password = 'yzckuxlnlbmelptj';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('menex68243@veb34.com');     // Add a recipient
+$mail->setFrom('aquamarintest@yandex.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('milyushina3@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
